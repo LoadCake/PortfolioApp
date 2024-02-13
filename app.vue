41dba0cd1ed1fb99267a1f8f333652549081b31f
LoadCake <stelod00@gmail.com>
2024-02-13 15:21:07 +0100

    Add basic design layout

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex items-stretch h-[100vh]">
+        <img class="grow min-w-0 object-cover mr-[-5rem]" src="/img/bash_tree.png">
+        <div class="flex">
+            <svg viewBox="0 0 200 200" preserveAspectRatio="none" class="shrink w-20 h-full border-r-5 border-slate-600 translate-x-px">
+                <polygon points="200,0 190,0 0,200 200,200" fill="#475569"/>
+            </svg>
+            <div class="grow bg-slate-600 flex flex-col items-center pt-20 pb-20 pl-20 pr-20 space-y-20 justify-center">
+                <button>Mastodon</button>
+                <button>Github</button>
+                <button>Linkedin</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
